c7ab6bfffbc59ba7016a258bdd56cc4d9cf062ad
AlissonRC <alissonrdacruz@gmail.com>
2022-04-23 20:46:39 -0300

    pagina produto inicio

--- js/index.js
+++ js/index.js
@@ -1,9 +1,45 @@
-const loadProducts = function () {
-  const page = document.body.id;
-  if (page === "produtos") {
-    const produtoDiv = document.getElementById("produto");
-    produtoDiv.innerHTML = "<div> Texto </div>";
-  }
-};
+function init() {
+  document.addEventListener("DOMContentLoaded", pageHandler());
+  console.log("HTML loaded");
+}
 
-loadProducts();
+function pageHandler() {
+  const page = document.body.id;
+  switch (page) {
+    case "produto":
+      loadProductsPage();
+      break;
+    case "carrinho":
+      something();
+      break;
+  }
+}
+
+function loadProductsPage() {
+  const itemStoredID = getItemStoredID();
+  console.log("estou na pagina de produto");
+  const productDetails = document.getElementById("produtoDetails");
+  console.log(productDetails);
+  productDetails.innerHTML = `<div> ${itemStoredID} </div>`;
+}
+
+function storeItemSelected(itemSelected) {
+  var item = itemSelected;
+  clearItemSelected();
+  if (!sessionStorage.getItem("itemSelected")) {
+    sessionStorage.setItem("itemSelected", item);
+  }
+
+  return true;
+}
+function getItemStoredID() {
+  const itemStoredID = sessionStorage.getItem("itemSelected");
+  console.log(sessionStorage.getItem("itemSelected"));
+  return itemStoredID;
+}
+
+function clearItemSelected() {
+  sessionStorage.removeItem("itemSelected");
+}
+
+init();
